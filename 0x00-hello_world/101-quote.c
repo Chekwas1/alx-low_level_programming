3668522d1fa6507611ccdaedfa96e7719fc791ae
onwuchekwa chetachukwu <smartgenuise806@gmail.com>
2022-09-08 20:21:13 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <unistd.h>
 
 /**
- * main - prints"and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * main - prints"and that piece of art is useful" - Dora Korpar, 2015-10-19":yh
  *     followed by a new line, to standard error.
  *
  * Return: Always 1
@@ -9,7 +9,9 @@
 int main(void)
 {
 	write(2,
-		"and that piece of art is useful\"-Dora Korpar, 2015-10-19\n", 
+		"and that piece of art is useful\"-
+		Dora Korpar, 2015-10-19\n",
 		59);
+
 	return (1);
 }
