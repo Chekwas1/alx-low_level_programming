1c64dc18cbb3ead6e5b385aaabf6d9729711a388
onwuchekwa chetachukwu <smartgenuise806@gmail.com>
2022-09-09 22:14:18 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,11 +10,10 @@
 
 int main(void)
 {
-	char low;
-
-	for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
-                putchar('\n');
-
-		return (0);
-}
+	char ch;
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('/n');
+	return (0);
