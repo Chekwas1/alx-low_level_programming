e74c6ac7262436cf2caec8dd37279ab991f50b1f
onwuchekwa chetachukwu <smartgenuise806@gmail.com>
2022-09-15 16:51:22 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints 0 - 9 except 2, 3
+ * Description: prints
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		if (a != 2 && a != 4)
+		{
+			continue;
+		}
+		putchar(n);
+	}
+	_putchar('\n');
+}
