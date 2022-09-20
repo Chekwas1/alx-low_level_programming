16a73e4e16ee45e51cb956ff429301fb7d041f25
onwuchekwa chetachukwu <smartgenuise806@gmail.com>
2022-09-19 20:49:04 -0700

    3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -9,9 +9,12 @@
 
 void _puts(char *str)
 {
-	char *str;
+	int i = 0;
 
-    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-    _puts(str);
-    return (0);
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
 }
