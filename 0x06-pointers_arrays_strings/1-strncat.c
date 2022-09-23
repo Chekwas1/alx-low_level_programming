e402d42020aa793234046aee3e492178fcf5ee8e
onwuchekwa chetachukwu <smartgenuise806@gmail.com>
2022-09-22 19:19:54 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,8 +12,7 @@ char *_strncat(char *dest, char *src, int n)
 	int cheta = 0, dest_len = 0;
 
 	while (dest[cheta++])
-		cheta++;
-
+		dest_len++;
 	for (cheta = 0; src[cheta] && cheta < n; cheta++)
 		dest[dest_len++] = src[cheta];
 	return (dest);
